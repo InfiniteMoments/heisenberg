b544e5db4c9522b16730da476e1cf53d54426cfb
Saikrishnan Ranganathan <saiaspire@gmail.com>
2015-02-09 17:12:15 +0800

    User validations change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
   before_save :downcase_email
   validates :name, presence: true, length: {maximum: 50}
+  validates :username, presence: true, length: {maximum: 20}, uniqueness: {case_sensitive: false}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
             format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, length: {minimum: 6}, allow_blank: true
+  validates :password, length: {minimum: 6}
   has_secure_password
 
   private
