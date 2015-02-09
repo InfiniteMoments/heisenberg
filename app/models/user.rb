96d332a1ede4744f7b7e6e8dc56c8ff9ac5243e8
Saikrishnan Ranganathan <saiaspire@gmail.com>
2015-02-09 20:56:57 +0800

    Add user model tests and update validations in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
-  before_save :downcase_email
+  before_save :downcase_email, :downcase_username
   validates :name, presence: true, length: {maximum: 50}
-  validates :username, presence: true, length: {maximum: 20}, uniqueness: {case_sensitive: false}
+
+  VALID_USERNAME_REGEX = /\A[\w\-]+\Z/
+  validates :username, presence: true, length: {maximum: 30},
+            format: {with: VALID_USERNAME_REGEX}, uniqueness: {case_sensitive: false}
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
             format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
   validates :password, length: {minimum: 6}
   has_secure_password
 
@@ -13,5 +18,10 @@ class User < ActiveRecord::Base
   # Converts email to all lower-case.
   def downcase_email
     self.email = email.downcase
+  end
+
+  # Converts username to all lower-case.
+  def downcase_username
+    self.username = username.downcase
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,113 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'Example User', email: 'user@example.com', username: 'exampleuser',
+                     password: 'foobar', password_confirmation: 'foobar')
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "  "
+    assert_not @user.valid?
+  end
+
+  test "username should be present" do
+    @user.username = "   "
+    assert_not @user.valid?
+  end
+
+  test "name can't be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email can't be too long" do
+    @user.email = "a" * 256
+    assert_not @user.valid?
+  end
+
+  test "username can't be too long" do
+    @user.username = "b" * 31
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email should be unique" do
+    dup_user = @user.dup
+    dup_user.username = "another_username"
+    dup_user.email.upcase!
+    @user.save
+    assert_not dup_user.valid?
+  end
+
+  test "username validation should accept valid usernames" do
+    valid_usernames = %w[exampleUser9 exampleuser momentsguy123 HelloMoments moments_best_user
+                         this-might-be-awesome_99 1awesome]
+    valid_usernames.each do |valid_username|
+      @user.username = valid_username
+      assert @user.valid?, "#{valid_username.inspect} should be valid"
+    end
+  end
+
+  test "username validation should reject invalid usernames" do
+    invalid_usernames = %w[b0$$ mi*1243 i\am\izzy hh/??AllStars
+                           i.am.awesome i@rocks i!!, #yolo]
+    invalid_usernames.each do |invalid_username|
+      @user.username = invalid_username
+      assert_not @user.valid?, "#{invalid_username.inspect} should be invalid"
+    end
+  end
+
+  test "username should be unique" do
+    dup_user = @user.dup
+    dup_user.email = "another_email@example.com"
+    dup_user.username.upcase!
+    @user.save
+    assert_not dup_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "username should be saved as lower-case" do
+    mixed_case_username = "eXampleUser"
+    @user.username = mixed_case_username
+    @user.save
+    assert_equal mixed_case_username.downcase, @user.reload.username
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
