114d4940e8c600fd1b8f5a411466ec5593b33452
Saikrishnan Ranganathan <saiaspire@gmail.com>
2015-02-12 22:31:40 +0800

    Add login and logout

---
+++ app/controllers/authentications_controller.rb
@@ -0,0 +1,17 @@
+class AuthenticationsController < ApplicationController
+  skip_before_action :validate_token, only: :create
+
+  def create
+    @user = User.find_by(username: params[:username].downcase)
+    if @user && @user.authenticate(params[:password])
+      @token = get_auth_token(@user)
+    else
+      render json: {error: 'Invalid username or password'}, status: :unauthorized
+    end
+  end
+
+  def destroy
+    # TODO Not final implementation
+    render json: {}
+  end
+end
